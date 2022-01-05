e7932e3ea0f0ebb4e11f8096adb1289627d25409
AntonioCardenas <antonioalx66@gmail.com>
2022-01-04 20:16:38 -0600

    start

---
+++ TestApp/TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Antonio Cardenas on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
